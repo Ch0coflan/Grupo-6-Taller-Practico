f3e8d8e459cec8738b3f0fa1f96ff35534930a2a
Santiago Alzate C <salzate361@misena.edu.co>
2024-06-04 16:56:22 -0500

    UI Melita

--- Assets/Scripts/Menu/Menu.cs
+++ Assets/Scripts/Menu/Menu.cs
@@ -1,29 +1,29 @@
 
-using System;
+
 using UnityEngine;
 
 public class Menu : MonoBehaviour
 {
     private MenuController _classMenuController;
-    public GameObject _prelobbyPanel;
-    public GameObject _creditsPanel;
-    public GameObject _optionsPanel;
-    public GameObject _lobbyHostPanel;
-    public GameObject _lobbyWestPanel;
-    public GameObject _lenguagePanel;
-    public GameObject _soundPanel;
-    public GameObject _pplPanel;
+    public GameObject prelobbyPanel;
+    public GameObject creditsPanel;
+    public GameObject optionsPanel;
+    public GameObject lobbyHostPanel;
+    public GameObject lobbyWestPanel;
+    public GameObject lenguagePanel;
+    public GameObject soundPanel;
+    public GameObject pplPanel;
     
     public void Start()
     {
-        _prelobbyPanel.SetActive(false);
-        _creditsPanel.SetActive(false);
-        _optionsPanel.SetActive(false);
-        _lenguagePanel.SetActive(false);
-        _soundPanel.SetActive(false);
-        _lobbyHostPanel.SetActive(false);
-        _lobbyWestPanel.SetActive(false);
-        _pplPanel.SetActive(true);
+        prelobbyPanel.SetActive(false);
+        creditsPanel.SetActive(false);
+        optionsPanel.SetActive(false);
+        lenguagePanel.SetActive(false);
+        soundPanel.SetActive(false);
+        lobbyHostPanel.SetActive(false);
+        lobbyWestPanel.SetActive(false);
+        pplPanel.SetActive(true);
     }
     
     
@@ -36,40 +36,50 @@ public class Menu : MonoBehaviour
     public void OnClickPlayButton()
     {
         // Desactiva el panel actual
-        _pplPanel.SetActive(false);
+        pplPanel.SetActive(false);
 
         // Activa el panel de Prelobby (con la etiqueta "Window2")
-        _prelobbyPanel.SetActive(true);
+        prelobbyPanel.SetActive(true);
         Debug.Log("Play button clicked");
     }
 
     public void OnClickCreditsButton()
     {
         // Desactiva el panel actual
-        _pplPanel.SetActive(false);
+        pplPanel.SetActive(false);
 
         // Activa el panel de Créditos (con la etiqueta "Window5")
-        _creditsPanel.SetActive(true);
+        creditsPanel.SetActive(true);
         Debug.Log("Credits button clicked");
     }
 
     public void OnClickOptionsButton()
     {
         // Desactiva el panel actual
-        _pplPanel.SetActive(false);
+        pplPanel.SetActive(false);
 
         // Activa el panel de Opciones (con la etiqueta "Window6")
-        _optionsPanel.SetActive(true);
+        optionsPanel.SetActive(true);
         Debug.Log("Options button clicked");
     }
 
+    public void GoBackPreLobbyButton()
+    {
+        // Activa el panel actual
+        prelobbyPanel.SetActive(false);
+        
+        // Desactiva el panel de Opciones (con la etiqueta "Window6")
+        pplPanel.SetActive(true);
+        
+        Debug.Log("Go Back Credits button clicked");
+    }
     public void GoBackOptionsButton()
     {
         // Activa el panel actual
-        _optionsPanel.SetActive(false);
+        optionsPanel.SetActive(false);
         
         // Desactiva el panel de Opciones (con la etiqueta "Window6")
-        _pplPanel.SetActive(true);
+        pplPanel.SetActive(true);
         
         Debug.Log("Go Back Options button clicked");
     }
@@ -77,11 +87,16 @@ public class Menu : MonoBehaviour
     public void GoBackCreditsButton()
     {
         // Activa el panel actual
-        _creditsPanel.SetActive(false);
+        creditsPanel.SetActive(false);
         
         // Desactiva el panel de Opciones (con la etiqueta "Window6")
-        _pplPanel.SetActive(true);
+        pplPanel.SetActive(true);
         
         Debug.Log("Go Back Credits button clicked");
+    }
+
+    public void QuitButton()
+    {
+        Application.Quit();
     }
 }
